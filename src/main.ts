46a3ea3d10a3045d7b6cdff4d6faf736cdd96c30
Galaxy97 <atnyehin@gmail.com>
2021-02-08 11:58:45 +0200

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 import { config } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = SwaggerModule.createDocument(app, config.constants.SWAGGER);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(config.env.PORT);
 }
 bootstrap();
